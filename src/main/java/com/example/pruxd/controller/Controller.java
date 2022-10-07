ca2c21466789cdf90c4603bc71d6e7706479598a
jacr1975 <jacr1975manizales@yahoo.com>
2022-10-07 09:36:18 -0500

    comm ter

--- src/main/java/com/example/pruxd/controller/Controller.java
+++ src/main/java/com/example/pruxd/controller/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
     String hello() {
 
 
-        String s = "Hello World, Spring Boot552315777!";
+        String s = "Hello World, Spring Boot552315777799999!";
         return s;
 
 
