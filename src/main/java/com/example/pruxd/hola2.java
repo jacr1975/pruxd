687cdfa332d3c1deffaf52443e1369b04d93cf5e
LAPTOP-7OQGSNPQ\jacr1 <jacr1975manizales@yahoo.com>
2023-07-12 07:29:21 -0500

    commit jul12 2 23

---
+++ src/main/java/com/example/pruxd/hola2.java
@@ -0,0 +1,14 @@
+package com.example.pruxd;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jacr1
+ */
+public class hola2 {
+    
+}
